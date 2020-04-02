e0add818f531eb7ce96d8448117f8b9af7fb8982
Vat Raghavan <machinshin@gmail.com>
2020-04-01 18:55:39 -0700

    1st commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Tarmon Gaidan has come & gone. This world is now safe from LeafBlighter.
+But this is not the only world. The Ogier are looking for a new world. a world of peace, 
+a world they hope to find somewhere on the other end of Ways.
+They have offerend to take with them any human who will be their scouts to this world of peace.
+A world that would be pure *stedding*
+
+
+Install:
+  1.  brew install giflib jpeg pango cairo pkg-config Gkk
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,46 @@
+/*
+}
+var user = "Vat Raghavan";
+console.log(greeter(user));
+
+
+const map = [
+// TODO: remove player, creatures, objects
+  '##+#########',
+  '#@....######',
+  '#...R.##...#',
+  '#.$$...##.w.#',
+  '###+####...#',
+  '###....=..w#',
+  '########...#',
+  '############',
+];
+
+// TODO: add for dimensions? or goto canvas
+const DrawMap = (levelGrid) => { 
+  console.log(levelGrid.map((row) => { 
+    return `$[row}\n`;
+  }));
+}
+*/
+
+var ac = require('ansi-canvas');
+
+var canvas = ac();
+var context = canvas.getContext('2d');
+
+// draw a purple background
+context.fillStyle = 'purple';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+// write some text
+context.fillStyle    = '#00f';
+context.font         = 'italic 15px sans-serif';
+context.textBaseline = 'top';
+context.fillText  ('Hello world!', 1, 1);
+context.font         = 'bold 20px sans-serif';
+context.strokeText('Hello world!', 1, 21);
+
+// IMPORTANT!!!
+// call `canvas.render()` when you're ready to flush the canvas to the terminal
+canvas.render();
