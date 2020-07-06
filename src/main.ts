22656f4fa971255a6c7b9fd17f7577063d06ea2d
Vat Raghavan <machinshin@gmail.com>
2020-07-06 16:40:15 -0700

    sync from local

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,4 @@
-/*
-}
-var user = "Vat Raghavan";
-console.log(greeter(user));
+const term = new Terminal();
+term.open(document.getElementById('terminal'));
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 
-
-const map = [
-// TODO: remove player, creatures, objects
-  '##+#########',
-  '#@....######',
-  '#...R.##...#',
-  '#.$$...##.w.#',
-  '###+####...#',
-  '###....=..w#',
-  '########...#',
-  '############',
-];
-
-// TODO: add for dimensions? or goto canvas
-const DrawMap = (levelGrid) => { 
-  console.log(levelGrid.map((row) => { 
-    return `$[row}\n`;
-  }));
-}
-*/
-
-var ac = require('ansi-canvas');
-
-var canvas = ac();
-var context = canvas.getContext('2d');
-
-// draw a purple background
-context.fillStyle = 'purple';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
-// write some text
-context.fillStyle    = '#00f';
-context.font         = 'italic 15px sans-serif';
-context.textBaseline = 'top';
-context.fillText  ('Hello world!', 1, 1);
-context.font         = 'bold 20px sans-serif';
-context.strokeText('Hello world!', 1, 21);
-
-// IMPORTANT!!!
-// call `canvas.render()` when you're ready to flush the canvas to the terminal
-canvas.render();
